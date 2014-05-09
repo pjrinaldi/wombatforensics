0fa7dee0007723e9381482d7dc5158a5d4ac3d7c
Pasquale Rinaldi <pjrinaldi@gmail.com>
2014-05-09 12:14:31 -0400

    trying to implement get file data so i can populate file contents in the hex editor

--- main/hexEditor.cpp
+++ main/hexEditor.cpp
@@ -96,6 +96,75 @@ bool HexEditor::openimage()
     calculateFontMetrics();
     setTopLeft(0);
 
+    // modify this function to account for a file, possibly by using it's offset???
+    // use icat_lib.c for reference..., icat.cpp for reference on how its called and what has to be loaded/found ahead of calling tsk_fs_icat()
+    /*
+     *static TSK_WALK_RET_ENUM
+icat_action(TSK_FS_FILE * fs_file, TSK_OFF_T a_off, TSK_DADDR_T addr,
+    char *buf, size_t size, TSK_FS_BLOCK_FLAG_ENUM flags, void *ptr)
+{
+    if (size == 0)
+        return TSK_WALK_CONT;
+
+    if (fwrite(buf, size, 1, stdout) != 1) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_FS_WRITE);
+        tsk_error_set_errstr("icat_action: error writing to stdout: %s",
+            strerror(errno));
+        return TSK_WALK_ERROR;
+    }
+
+    return TSK_WALK_CONT;
+}
+
+// Return 1 on error and 0 on success //
+uint8_t
+tsk_fs_icat(TSK_FS_INFO * fs, TSK_INUM_T inum,
+    TSK_FS_ATTR_TYPE_ENUM type, uint8_t type_used,
+    uint16_t id, uint8_t id_used, TSK_FS_FILE_WALK_FLAG_ENUM flags)
+{
+    TSK_FS_FILE *fs_file;
+
+#ifdef TSK_WIN32
+    if (-1 == _setmode(_fileno(stdout), _O_BINARY)) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_FS_WRITE);
+        tsk_error_set_errstr
+            ("icat_lib: error setting stdout to binary: %s",
+            strerror(errno));
+        return 1;
+    }
+#endif
+
+    fs_file = tsk_fs_file_open_meta(fs, NULL, inum);
+    if (!fs_file) {
+        return 1;
+    }
+
+    if (type_used) {
+        if (id_used == 0) {
+            flags |= TSK_FS_FILE_WALK_FLAG_NOID;
+        }
+        if (tsk_fs_file_walk_type(fs_file, type, id, flags, icat_action,
+                NULL)) {
+            tsk_fs_file_close(fs_file);
+            return 1;
+        }
+    }
+    else {
+        if (tsk_fs_file_walk(fs_file, flags, icat_action, NULL)) {
+            tsk_fs_file_close(fs_file);
+            return 1;
+        }
+    }
+
+
+    tsk_fs_file_close(fs_file);
+
+    return 0;
+     *
+     */ 
+
     return true;
 }
 
