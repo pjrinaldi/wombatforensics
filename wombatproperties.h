c25385a40f593a6faff3f8aeefd3bd06b9e9985a
Pasquale Rinaldi <pjrinaldi@gmail.com>
2014-09-09 21:00:29 -0400

    working on the migration of fs specific code to the wombatproperties file

--- wombatproperties.h
+++ wombatproperties.h
@@ -13,7 +13,7 @@ public:
     QString GetFileSystemLabel(void);
     QStringList PopulateEvidenceImageProperties(void);
     QStringList PopulatePartitionProperties(void);
-    QStringList PopulateFileSystemProperties(void);
+    QStringList PopulateFileSystemProperties(TSK_FS_INFO* curfsinfo);
     QStringList PopulateFileProperties(void);
 
 private:
@@ -28,6 +28,45 @@ private:
     uint64_t value64bit;
     size64_t size64bit;
     libewf_error_t* ewferror;
+    /*
+    TSK_FS_FILE* tmpfile = NULL;
+    ntfs_sb* ntfssb = NULL;
+    FFS_INFO* ffs = NULL;
+    ffs_sb1* sb1 = NULL;
+    ffs_sb2* sb2 = NULL;
+    FATXXFS_DENTRY* tmpfatdentry = NULL;
+    FATXXFS_DENTRY* curentry = NULL;
+    FATFS_INFO* fatfs = NULL;
+    FATXXFS_SB* fatsb = NULL;
+    const TSK_FS_ATTR*tmpattr;
+    TSK_DADDR_T cursector = 0;
+    TSK_DADDR_T endsector = 0;
+    int8_t isallocsec = 0;
+    TSK_INUM_T curinum = 0;
+    */
+    //FATFS_DENTRY* dentry = NULL;
+    EXT2FS_INFO* ext2fs = NULL;
+    /*
+    ISO_INFO* iso = NULL;
+    iso9660_pvd_node* p = NULL;
+    iso9660_svd_node* s = NULL;
+    HFS_INFO* hfs = NULL;
+    hfs_plus_vh* hsb = NULL;
+    char fn[HFS_MAXNAMLEN + 1];
+    HFS_ENTRY* hfsentry = NULL;
+    hfs_btree_key_cat key;
+    hfs_thread thread;
+    hfs_file_folder record;
+    TSK_OFF_T off;
+    char* databuffer = NULL;
+    ssize_t cnt;
+    ssize_t bytesread = 0;
+    int a;
+    uint len = 0;
+    char asc[512];
+    char timebuf[128];
+    */
+
 };
 
 #endif // WOMBATPROPERTIES_H
