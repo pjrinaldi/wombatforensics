ae0aae02670e87e86228d21ade5665ede10d8d94
Pasquale Rinaldi <pjrinaldi@gmail.com>
2014-10-16 08:27:46 -0400

    working on hfs+. what a pita it is.

--- wombatproperties.h
+++ wombatproperties.h
@@ -5,6 +5,7 @@
 #include "wombatvariable.h"
 #include "wombatfunctions.h"
 
+
 class WombatProperties : public QObject
 {
     Q_OBJECT
@@ -18,7 +19,15 @@ public:
     QStringList PopulateFileProperties(void);
     QString ConvertGmtHours(int gmtvar);
     void yaffscache_objects_stats(YAFFSFS_INFO* yfs, unsigned int* objcnt, uint32_t* objfirst, uint32_t* objlast, uint32_t* vercnt, uint32_t* verfirst, uint32_t* verlast);
+    uint8_t hfs_UTF16toUTF8(TSK_FS_INFO* fs, uint8_t* uni, int ulen, char* asc, int alen, uint32_t flags);
+    static int hfs_cat_compare_keys(HFS_INFO* hfs, const hfs_btree_key_cat* key1, const hfs_btree_key_cat* key2);
+    static uint8_t hfs_cat_traverse(HFS_INFO* hfs, const void* targ_data, TSK_HFS_BTREE_CB a_cb, void* ptr);
     uint32_t hfs_convert_2_unix_time(uint32_t hfsdate);
+    uint8_t hfs_cat_file_lookup(HFS_INFO* hfs, TSK_INUM_T inum, HFS_ENTRY* entry, unsigned char follow_hard_link);
+    static uint8_t hfs_cat_get_record_offset_cb(HFS_INFO* hfs, int8_t level_type, const void* targ_data, const hfs_btree_key_cat* cur_key, TSK_OFF_T key_off, void* ptr);
+    static TSK_OFF_T hfs_cat_get_record_offset(HFS_INFO* hfs, const hfs_btree_key_cat* needle);
+    uint8_t hfs_cat_read_thread_record(HFS_INFO* hfs, TSK_OFF_T off, hfs_thread* thread);
+    uint8_t hfs_cat_read_file_folder_record(HFS_INFO* hfs, TSK_OFF_T off, hfs_file_folder* record);
 
 private:
     WombatVariable* wombatptr;
