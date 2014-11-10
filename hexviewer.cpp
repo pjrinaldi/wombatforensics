66dba98b1428df5961cbae39024dcdd9d328d8b9
Pasquale Rinaldi <pjrinaldi@gmail.com>
2014-11-10 09:00:21 -0500

    working on hexviewer reimplementation

--- hexviewer.cpp
+++ hexviewer.cpp
@@ -14,4 +14,174 @@ void HexViewer::keyPressEvent(QKeyEvent* e)
     int charx = (cursorx - hexposition)/charwidth;
     int posx = (charx/3)*2 + (charx % 3);
     int posba = (cursory/charheight) * BYTES_PER_LINE + posx/2;
+
+    // CURSOR MOVEMENTS
+    if(e->matches(QKeySequence::MoveToNextChar))
+    {
+        SetCursorPosition(cursorposition+1);
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToPreviousChar))
+    {
+        SetCursorPosition(cursorposition-1);
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToEndOfLine))
+    {
+        SetCursorPosition(cursorposition | (2*BYTES_PER_LINE - 1));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToStartOfLine))
+    {
+        SetCursorPosition(cursorposition - (cursorposition % (2*BYTES_PER_LINE)));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToPreviousLine))
+    {
+        SetCursorPosition(cursorposition - (2 * BYTES_PER_LINE));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToNextLine))
+    {
+        SetCursorPosition(cursorposition + (2 * BYTES_PER_LINE));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToNextPage))
+    {
+        SetCursorPosition(cursorposition + (((scrollarea->viewport()->height() / charheight) - 1) * 2 * BYTES_PER_LINE));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToPreviousPage))
+    {
+        SetCursorPosition(cursorposition - (((scrollarea->viewport()->height() / charheight) - 1) * 2 * BYTES_PER_LINE));
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToEndOfDocument))
+    {
+        SetCursorPosition(bytedata.size() * 2);
+        ResetSelection(cursorposition);
+    }
+    if(e->matches(QKeySequence::MoveToStartOfDocument))
+    {
+        SetCursorPosition(0);
+        ResetSelection(cursorposition);
+    }
+    // SELECT COMMANDS
+    if (e->matches(QKeySequence::SelectAll))
+    {
+        ResetSelection(0);
+        SetSelection(2*bytedata.size() + 1);
+    }
+    if (e->matches(QKeySequence::SelectNextChar))
+    {
+        int pos = cursorposition + 1;
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectPreviousChar))
+    {
+        int pos = cursorposition - 1;
+        SetSelection(pos);
+        SetCursorPosition(pos);
+    }
+    if (e->matches(QKeySequence::SelectEndOfLine))
+    {
+        int pos = cursorposition - (cursorposition % (2 * BYTES_PER_LINE)) + (2 * BYTES_PER_LINE);
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectStartOfLine))
+    {
+        int pos = cursorposition - (cursorposition % (2 * BYTES_PER_LINE));
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectPreviousLine))
+    {
+        int pos = cursorposition - (2 * BYTES_PER_LINE);
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectNextLine))
+    {
+        int pos = cursorposition + (2 * BYTES_PER_LINE);
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+
+    if (e->matches(QKeySequence::SelectNextPage))
+    {
+        int pos = cursorposition + (((scrollarea->viewport()->height() / charheight) - 1) * 2 * BYTES_PER_LINE);
+        setCursorPos(pos);
+        setSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectPreviousPage))
+    {
+        int pos = cursorposition - (((scrollarea->viewport()->height() / charheight) - 1) * 2 * BYTES_PER_LINE);
+        setCursorPos(pos);
+        setSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectEndOfDocument))
+    {
+        int pos = bytedata.size() * 2;
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    if (e->matches(QKeySequence::SelectStartOfDocument))
+    {
+        int pos = 0;
+        SetCursorPosition(pos);
+        SetSelection(pos);
+    }
+    EnsureVisible();
+    update();
+}
+
+void HexViewer::mouseMoveEvent(QMouseEvent* e)
+{
+    update();
+    int actpos = CursorPosition(e->pos());
+    SetCursorPosition(actpos);
+    SetSelection(actpos);
+}
+
+void HexViewer::mousePressEvent(QMouseEvent* e)
+{
+    update();
+    int cpos = CursorPosition(e->pos());
+    ResetSelection(cpos);
+    SetCursorPosition(cpos);
+}
+
+void HexViewer::paintEvent(QPaintEvent* e)
+{
+    QPainter painter(this);
+    painter.fillRect(e->rect(), this->palette().color(QPalette::Base));
+    painter.fillRect(QRect(labelposition, e->rect().top(), hexposition - GAP_ADR_HEX + 2, height()), addressareacolor);
+    int lineposition = asciiposition - (GAP_HEX_ASCII/2);
+    painter.setPen(Qt::gray);
+    painter.drawLine(lineposition, e->rect().top(), lineposition, height());
+
+    painter.setPen(this->palette.color(QPalette::WindowText));
+
+    int firstlineindex = ((e->rect().top()/charheight) - charheight) * BYTES_PER_LINE;
+    if(firstlineindex < 0)
+        firstlineindex = 0;
+    int lastlineindex = ((e->rect().botto()/charheight) + charheight) * BYTES_PER_LINE;
+    if(lastlineindex > bytedata.size())
+        lastlineindex = bytedata.size();
+    int yposstart = ((firstlineindex)/BYTES_PER_LINE) * charheight + charheight;
+
+    // PAINT ADDRESS AREA
+    for(int lineindex = firstlineindex, ypos = yposstart; lineindex < lastlineindex; lindeindex += BYTES_PER_LINE, ypos += charheight)
+    {
+        QString address = QString("%1").arg(lineindex + xdata.addressOffset(), xdata.realAddressNumbers(), 16, QChar('0'));
+        painter.drawText(xposaddr, ypos, address);
+    }
+
+    // PAINT HEX AREA
+
+    // PAINT ASCII AREA
+
+    // PAINT CURSOR
 }
--- hexviewer.h
+++ hexviewer.h
@@ -2,9 +2,12 @@
 #define HEXVIEWER_H
 
 #include <QtCore>
+#include "xbytearray.h"
 
 class HexViewer : public QScrollArea
 {
+    QOBJECT
+
 public:
     int AddressOffset(void);
     void SetAddressOffset(int offset);
@@ -20,6 +23,9 @@ public:
 
     QChar AsciiChar(int index);
 
+    void SetCursorPosition(int position);
+    int GetCursorPosition(void);
+
 signals:
     void CurrentAddressChanged(int address);
     void DataChanged(void);
@@ -29,10 +35,16 @@ protected:
     void mousePressEvent(QMouseEvent* event);
     void paintEvent(QPaintEvent* event);
     int CursorPosition(QPoint pos);
+    void ResetSelection(int pos);
+    void ResetSelection(void);
+    void SetSelection(int pos);
+    int GetSelectionBegin(void);
+    int GetSelectionEnd(void);
 private slots:
     void UpdateCursor(void);
 private:
-    QByteArray data;
+    QByteArray bytedata;
+    XByteArray xdata;
     int addressnumbers;
     int addressoffset;
     int realaddressnumbers;
@@ -45,6 +57,8 @@ private:
     int selectionstart;
     int selectionend;
     int selectioninitial;
+
+    void EnsureVisible(void);
 
 };
 
