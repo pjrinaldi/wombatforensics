30f6282eafabd99de4182aa28021947e79205929
Pasquale Rinaldi <pjrinaldi@gmail.com>
2015-01-01 12:18:58 -0500

    there is cross code between the fileviewer offset and the hexviewer offset. also, the fileviewer scrolling isn't working.

--- fileviewer.cpp
+++ fileviewer.cpp
@@ -32,6 +32,21 @@ FileViewer::FileViewer(QWidget* parent, TskObject* tskobjptr) : QMainWindow(pare
     connect(filehexvsb, SIGNAL(valueChanged(int)), this, SLOT(AdjustData(int)));
     connect(filehexview, SIGNAL(selectionChanged(const QString &)), this, SLOT(UpdateSelectValue(const QString&)));
     connect(filehexview, SIGNAL(StepValues(int, int)), this, SLOT(SetStepValues(int, int)));
+    //filehexview->openimage();
+
+
+    //qDebug() << "reader size: " << _reader.size();
+    //_cursor.setRange(0, _reader.size());
+    //_cursor.setCharsPerByte(_charsPerByte);
+    //setSelection(SelectionStart, -1);
+    //setSelection(SelectionEnd, -1);
+    //emit rangeChanged(0, _reader.size()/bytesPerLine());
+    //emit StepValues(bytesPerLine(), bytesPerPage());
+    //emit StepValues(bytesPerLine(), bytesPerPage()/bytesPerLine());
+    //emit StepValues(1, bytesPerPage()/bytesPerLine());
+    //calculateFontMetrics();
+    //setTopLeft(0);
+
 }
 
 FileViewer::~FileViewer()
@@ -175,7 +190,8 @@ void FileViewer::LoadPage(off_t pageindex)
     /*
     --filereader->nFreePages();
     */
-    retval = tsk_fs_file_read(tskptr->readfileinfo, tskptr->offset + pageindex*filereader->_pageSize, (char*)filedata[pageindex], filereader->_pageSize, TSK_FS_FILE_READ_FLAG_SLACK);
+    retval = tsk_fs_file_read(tskptr->readfileinfo, pageindex*filereader->_pageSize, (char*)filedata[pageindex], filereader->_pageSize, TSK_FS_FILE_READ_FLAG_SLACK);
+    //retval = tsk_fs_file_read(tskptr->readfileinfo, tskptr->offset + pageindex*filereader->_pageSize, (char*)filedata[pageindex], filereader->_pageSize, TSK_FS_FILE_READ_FLAG_SLACK);
     /*
     if(retval > 0)
     {
