9aaff952168377378ca5842ddbde36c7e371a5a9
Pasquale Rinaldi <pjrinaldi@gmail.com>
2015-01-25 14:35:42 -0500

    working on migrating to model/view method since listwidget sucks.

--- imageviewer.h
+++ imageviewer.h
@@ -5,6 +5,69 @@
 #include "globals.h"
 #include "ui_imageviewer.h"
 
+/*
+ *
+class PropertyModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    PropertyModel(const QStringList plist, QObject* parent = 0) : QAbstractTableModel(parent), proplist(plist)
+    {
+    };
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const
+    {
+        if(parent.row() >= proplist.count()/3)
+            return 0;
+        return ((int)proplist.count())/((int)3);
+    };
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const
+    {
+        if(parent.row() >= proplist.count()/3)
+            return 0;
+        return 3;
+    }
+
+    QVariant data(const QModelIndex &index, int role) const
+    {
+        if(!index.isValid())
+            return QVariant();
+        if(index.row() >= proplist.count()/3)
+            return QVariant();
+        if(role == Qt::DisplayRole)
+        {
+            if(index.column() == 0)
+                return proplist.at(3*index.row());
+            else if(index.column() == 1)
+                return proplist.at(3*index.row() + 1);
+            else if(index.column() == 2)
+                return proplist.at(3*index.row() + 2);
+            else
+                return QVariant();
+        }
+        else
+            return QVariant();
+    };
+
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const
+    {
+        QStringList headerdata;
+        headerdata << "Property" << "Value" << "Description";
+        if(role != Qt::DisplayRole)
+            return QVariant();
+        if(orientation == Qt::Horizontal)
+            return headerdata.at(section);
+        return QVariant();
+    };
+
+private:
+    QStringList proplist;
+};
+
+ *
+ */ 
 namespace Ui
 {
     class ImageViewer;
