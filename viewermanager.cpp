7733d0a68872c712d74b4cbddc12dc1017254b56
Pasquale Rinaldi <pjrinaldi@gmail.com>
2015-02-01 19:19:36 -0500

    code cleanup.

--- viewermanager.cpp
+++ viewermanager.cpp
@@ -8,7 +8,6 @@ ViewerManager::ViewerManager(QWidget* parent) : QDialog(parent), ui(new Ui::View
     connect(ui->browsebutton, SIGNAL(clicked()), this, SLOT(ShowBrowser()));
     connect(ui->addbutton, SIGNAL(clicked()), this, SLOT(AddViewer()));
     connect(ui->removebutton, SIGNAL(clicked()), this, SLOT(RemoveSelected()));
-    // query for any external viewers and add here if they exist.
     if(!fappdb.isOpen())
         fappdb.open();
     QSqlQuery equery(fappdb);
@@ -21,7 +20,6 @@ ViewerManager::ViewerManager(QWidget* parent) : QDialog(parent), ui(new Ui::View
     equery.finish();
     viewmodel = new ViewerModel(externlist);
     ui->listView->setModel(viewmodel);
-    // ERROR WITH NO SUCH SIGNAL
     connect(ui->listView->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), this, SLOT(SelectionChanged(const QItemSelection &, const QItemSelection &)));
 }
 
@@ -56,61 +54,13 @@ void ViewerManager::ShowBrowser()
 void ViewerManager::AddViewer()
 {
     viewmodel->AddViewer(fileviewerpath);
-    //bool viewexists = false;
-    //int isdeleted = 0;
-    //QSqlQuery existquery(fappdb);
-    //existquery.prepare("SELECT path, deleted FROM externalviewers;");
-    //existquery.exec();
-    /*
-    if(existquery.size() > 0)
-    {
-        while(existquery.next())
-        {
-            if(fileviewerpath.compare(existquery.value(0).toString()) == 0)
-            {
-                viewexists = true;
-                isdeleted = existquery.value(1).toInt();
-            }
-        }
-    }
-    existquery.finish();
-    if(viewexists == true)
-    {
-        if(isdeleted == 1)
-        {
-            existquery.prepare("UPDATE externalviewers SET deleted = 0 WHERE path = ?;");
-            existquery.addBindValue(fileviewerpath);
-            existquery.exec();
-            existquery.next();
-            existquery.finish();
-        }
-    }
-    else
-    {
-        existquery.prepare("INSERT INTO externalviewers (path, deleted) VALUES(?, 0);");
-        existquery.addBindValue(fileviewerpath);
-        existquery.exec();
-        existquery.next();
-        existquery.finish();
-    }
-    */
     ui->lineEdit->setText("");
     ui->addbutton->setEnabled(false);
-    // may need to reset the model or update it when the new values are added.
-    //ui->listView->reset();
 }
 
 void ViewerManager::RemoveSelected()
 {
     viewmodel->RemoveSelected(selectedindex);
-    /*
-    QSqlQuery remquery(fappdb);
-    remquery.prepare("UPDATE externalviewers SET deleted = 1 WHERE path = ?;");
-    remquery.addBindValue(selectedindex.sibling(selectedindex.row(), 0).data().toString());
-    remquery.exec();
-    remquery.next();
-    remquery.finish();
-    */
     ui->removebutton->setEnabled(false);
 }
 
@@ -119,31 +69,10 @@ void ViewerManager::SelectionChanged(const QItemSelection &newitem, const QItemS
     if(olditem.indexes().count() > 0)
     {
     }
-       //QModelIndex oldselectedindex = olditem.indexes().at(0);
     if(newitem.indexes().count() > 0)
     {
         selectedindex = newitem.indexes().at(0);
         ui->removebutton->setEnabled(true);
 
     }
-    /*
-     *    if(previtem.indexes().count() > 0)
-        oldselectedindex = previtem.indexes().at(0);
-    if(curitem.indexes().count() > 0)
-    {
-        selectedindex = curitem.indexes().at(0);
-        ui->actionView_Properties->setEnabled(true);
-        ui->actionView_File->setEnabled(true);
-        ui->actionView_Image_Gallery->setEnabled(true);
-        ui->actionExport_Evidence->setEnabled(true);
-        wombatvarptr->selectedobject.id = selectedindex.sibling(selectedindex.row(), 0).data().toInt(); // object id
-        wombatvarptr->selectedobject.name = selectedindex.sibling(selectedindex.row(), 1).data().toString(); // object name
-        wombatdatabase->GetObjectValues(); // now i have selectedobject.values.
-        LoadHexContents();
-        if(propertywindow->isVisible())
-            UpdateProperties();
- 
-     *
-     */ 
-    // update selected item so i know what might get removed. i need to query the name. loop where it contains.
 }
--- wombatforensics.h
+++ wombatforensics.h
@@ -426,15 +426,6 @@ public:
             }
         }
     };
-/*
-    void sort(int col, Qt::SortOrder order = Qt::AscendingOrder)
-    {
-        // i won't be using sorting, so i can probably delete this.
-        if(col == 3) // file size sort
-        {
-            qDebug() << "sorting would work now if i could figure it out.";
-        }
-    };*/
     
     void GetModelCount(Node* curnode)
     {
