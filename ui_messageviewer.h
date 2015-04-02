e261d624cf57e6328e9fec2d7109ad8747d0ca21
Pasquale Rinaldi <pjrinaldi@gmail.com>
2015-04-02 13:32:15 -0400

    changes to imageviewer ui

--- ui_messageviewer.h
+++ ui_messageviewer.h
@@ -68,6 +68,11 @@ public:
     {
         MessageViewer->setWindowTitle(QApplication::translate("MessageViewer", "Message Viewer", 0));
         checkBox->setText(QApplication::translate("MessageViewer", "View Logged Messages", 0));
+        textEdit->setHtml(QApplication::translate("MessageViewer", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Bitstream Vera Sans'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", 0));
         textEdit->setPlaceholderText(QApplication::translate("MessageViewer", "Messages and Errors go here...", 0));
     } // retranslateUi
 
