4d4163cbb9545fb45303f2d5de00922dd3996c9f
Pasquale Rinaldi <pjrinaldi@gmail.com>
2018-09-14 11:54:12 -0400

    modified about information

--- aboutbox.cpp
+++ aboutbox.cpp
@@ -6,12 +6,12 @@
 AboutBox::AboutBox(QWidget* parent) : QDialog(parent), ui(new Ui::AboutBox)
 {
     ui->setupUi(this);
-    QString abouttext = "<h3>About WombatForensics v1.0</h3>";
+    QString abouttext = "<h3>About WombatForensics v0.1</h3>";
     abouttext += "<h5>License: GPLv2</h5>";
     abouttext += "<4>Copyright 2015 Pasquale J. Rinaldi, Jr.</h4>";
-    abouttext += "<h5>Email: pjrinaldi@gmail.com&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Phone: (865) 206-8130</h5>";
-    abouttext += "<p>This program incorporates modified code from the lfhex project to implement the hexviewers.";
-    abouttext += "lfhex is provided under the gplv2. I also used Qt5.5, the sleuthkit c/c++ functions, and the treepata icons.";
+    abouttext += "<h5>Email: pjrinaldi@gmail.com</h5>";
+    abouttext += "<p>This program incorporates modified code from the qhexedit2 project to implement the hexviewers.";
+    abouttext += "qhexedit2 is provided under the gplv2.1. I also used Qt5.11, the sleuthkit c/c++ functions, and the treepata icons.";
     abouttext += "The wombat icon was designed by Lindsey Pargman and a lot of the design implementation ideas were brainstormed with Mark Smith.</p>";
     ui->label->setText(abouttext);
     this->hide();
