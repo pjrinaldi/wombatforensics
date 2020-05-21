5773f2b4b05bcdc3eebc0be4cf1458c02923886c
Pasquale Rinaldi <pjrinaldi@gmail.com>
2020-05-21 01:39:41 +0000

    added some files for testing and reference for the library...

---
+++ prefetch.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ios>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+#include <time.h>
+#include "/usr/local/include/libscca.h"
+
+std::string PrefetchParse(std::string prefetchfilestring);
+
