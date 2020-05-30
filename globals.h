2652b2ebd74493315acce321dae5ab2ba22cdbb8
Pasquale Rinaldi <pjrinaldi@gmail.com>
2020-05-30 20:54:01 +0000

    code cleanup, hope orphandirs works this time

--- globals.h
+++ globals.h
@@ -483,41 +483,6 @@ public:
                     if(itemtype == 3)
                         return QIcon(":/basic/virtualfolder");
                     return QIcon(":/basic/carveicon");
-                    /*
-                    if((itemtype == 0 && nodename.contains("$OrphanFiles")) || itemtype == 5) // used to be (2)
-                    {
-                        if(nodename.compare("AttrDef") == 0 || nodename.compare("$BadClus") == 0 || nodename.compare("$Bitmap") == 0 || nodename.compare("$Boot") == 0 || nodename.compare("$ObjId") == 0 || nodename.compare("$Quota") == 0 || nodename.compare("$Reparse") == 0 || nodename.compare("$LogFile") == 0 || nodename.compare("$MFT") == 0 || nodename.compare("$MFTMirr") == 0 || nodename.compare("$Secure") == 0 || nodename.compare("$UpCase") == 0 || nodename.compare("$Volume") == 0)
-                            return QIcon(":/basic/virtualfile");
-                        else if(nodename.compare("$OprhanFiles"))
-                            return QIcon(":/basic/virtualfolder");
-                        else
-                        {
-                            if(itemnode->IsDeleted()) // Unallocated and Used
-                                return QIcon(":/basic/deletedfile");
-                            else
-                                return QIcon(":/basic/treefile");
-                        }
-                    }
-                    else if(itemtype == 3)
-                    {
-                        if(nodename.compare("$OrphanFiles") == 0 || nodename.compare("$Extend") == 0 || nodename.compare("$Carved") == 0)
-                            return QIcon(":/basic/virtualfolder");
-                        else
-                            return QIcon(":/basic/treefolder");
-                    }
-                    else if(itemtype == 10)
-                        return QIcon(":/basic/virtualfile");
-                    else if(itemtype == 11)
-                        return QIcon(":/basic/virtualdir");
-                    else if(itemtype == 15)
-                        return QIcon(":/basic/carveicon");
-                    else
-                    {
-                        if(itemnode->Data(11).toString().contains("fd") || itemnode->Data(11).toString().contains("fo")) // used to be 0
-                            return QIcon(":/basic/deletedfile");
-                        else
-                            return QIcon(":/basic/treefile");
-                    }*/
                 }
                 else if(nodetype == 5)
                 {
