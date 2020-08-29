6b34669d3283c9b958e26e0aa4ca0b0a75712861
Pasquale Rinaldi <pjrinaldi@gmail.com>
2020-08-29 20:00:35 +0000

    working on registry values

--- registryviewer.cpp
+++ registryviewer.cpp
@@ -113,9 +113,15 @@ void RegistryDialog::ValueSelected(void)
                     uint8_t data[datasize];
                     libregf_value_get_value_data(curval, data, datasize, &regerr);
                     QByteArray farray = QByteArray::fromRawData((char*)data, datasize);
-                    //uint64_t createdtime = qFromLittleEndian<uint64_t>(filenameattribute.mid(8, 8));
+                    valuedata += "Account Expiration:\t\t";
+                    if(farray.at(32) == 0xff)
+                        valuedata += "No Expiration\n";
+                    else
+                        valuedata += ConvertWindowsTimeToUnixTimeUTC(qFromLittleEndian<uint64_t>(farray.mid(32, 8))) + " UTC\n";
+                    valuedata += "Last Logon Time:\t\t" + ConvertWindowsTimeToUnixTimeUTC(qFromLittleEndian<uint64_t>(farray.mid(8, 8))) + " UTC\n";
+                    valuedata += "Last Failed Login:\t\t" + ConvertWindowsTimeToUnixTimeUTC(qFromLittleEndian<uint64_t>(farray.mid(40, 8))) + " UTC\n";
+                    valuedata += "Last Time Password Changed:\t\t" + ConvertWindowsTimeToUnixTimeUTC(qFromLittleEndian<uint64_t>(farray.mid(24, 8))) + " UTC";
 	            //QString filenamestring = QString::fromStdString(QByteArray(info2content.mid(curpos + 3, 260).toStdString().c_str(), -1).toStdString());
-	            //htmlstr += "<tr class=odd><td class=aval>Last Run Time:</td><td>" + ConvertWindowsTimeToUnixTime(qFromLittleEndian<uint64_t>(fileinformation.mid(36, 8))) + "</td></tr>";
                 }
                 else if(keypath.contains("SAM") && ui->tableWidget->selectedItems().first()->text().count() == 1 && ui->tableWidget->selectedItems().first()->text().startsWith("V"))
                 {
