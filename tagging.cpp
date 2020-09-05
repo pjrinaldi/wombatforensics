ef28b89d9c2560f2cdd644870bc9a272803739a2
Pasquale Rinaldi <pjrinaldi@gmail.com>
2020-09-05 15:37:23 +0000

    working on independent tagging code so i can more easily incorporate tagging into registry and other viewers.

---
+++ tagging.cpp
@@ -0,0 +1,4 @@
+#include "tagging.h"
+
+// Copyright 2013-2020 Pasquale J. Rinaldi, Jr.
+// Distrubted under the terms of the GNU General Public License version 2
---
+++ tagging.h
@@ -0,0 +1,11 @@
+#ifndef TAGGING_H
+#define TAGGING_H
+
+#include "wombatinclude.h"
+#include "globals.h"
+//#include "tskcpp.h"
+
+// Copyright 2013-2020 Pasquale J. Rinaldi, Jr.
+// Distrubted under the terms of the GNU General Public License version 2
+
+#endif // TAGGING_H
