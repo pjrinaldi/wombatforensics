89d6024e28e62dffe6669c1f17a40a3b733f767f
Pasquale Rinaldi <pjrinaldi@gmail.com>
2021-05-01 23:57:02 +0000

    working on v0.4 release

--- wombatforensics.cpp
+++ wombatforensics.cpp
@@ -1,9 +1,9 @@
 #include "wombatforensics.h"
-#include "fusefunctions.h"
-#include "affuse.h"
-#include "ewffuse.h"
+//#include "fusefunctions.h"
+//#include "affuse.h"
+//#include "ewffuse.h"
 //#include "sqfuse.h"
-#include "zmgfuse.h"
+//#include "zmgfuse.h"
 
 // Copyright 2013-2020 Pasquale J. Rinaldi, Jr.
 // Distrubted under the terms of the GNU General Public License version 2
