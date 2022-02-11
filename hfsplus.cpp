78557249bfeec4f8cfc9025b6cb1ea242cb680c3
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-02-11 00:44:45 +0000

    i got the processing figured out... there might be an issue with a large catalog file and the split where i need to add the curoffset/curpos/etc to the split, but otherwise it is working. Now i just need to parse the contents, store them in a node and prop file and see what i get...

--- hfsplus.cpp
+++ hfsplus.cpp
@@ -33,12 +33,14 @@ qulonglong ParseHfsPlusDirectory(ForImg* curimg, uint32_t curstartsector, uint8_
         }
         propfile.close();
     }
+    /*
     qDebug() << "cluster size:" << clustersize;
     qDebug() << "catalogs logical size:" << catalogsize;
     qDebug() << "catalogtotalblocks:" << catalogtotalblocks;
     qDebug() << "catalogextstartblockarray:" << catalogextstartblockarray;
     qDebug() << "catalogextblockcntarray:" << catalogextblockcntarray;
     qDebug() << "start block cnt:" << catalogextstartblockarray.split(",", Qt::SkipEmptyParts).count();
+    */
 
     uint16_t nodesize = 0;
     quint64 curoffset = 0;
@@ -62,9 +64,71 @@ qulonglong ParseHfsPlusDirectory(ForImg* curimg, uint32_t curstartsector, uint8_
             // SO FOR THIS EXAMPLE, IT IS (2-1) * 4096, NEED TO SET THE OFFSET TO BE 
             curoffset += nodesize; // SHOULD PUT ME AT 4096, START OF THE CATALOG ROOT DIRECTORY NODE
             // READING THE NODE DESCRIPTOR IS ONLY IN THE i=0 PORTION OF THE CATALOG ROOT DIRECTORY
-            qDebug() << curimg->ReadContent(curoffset, 14).toHex();
-            cufoffset += 14; // THIS PUTS THE CUROFFSET INTO THE KEY/DATA LOOP FOR THE CATALOG LEAF NODE, SO ITERATING SHOULD WORK HERE
+            // GET THE NUMBER OF RECORDS
+            uint16_t recordcount = qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + 10, 2));
+            qDebug() << "recordcount:" << recordcount; // MIGHT BE ANOTHER WAY TO LOOP OVER THE RECORDS, RATHER THAN THE CUROFFSET + CURPOS METHOD...
+            //qDebug() << curimg->ReadContent(curoffset, 14).toHex();
+            //curoffset += 14; // THIS PUTS THE CUROFFSET INTO THE KEY/DATA LOOP FOR THE CATALOG LEAF NODE, SO ITERATING SHOULD WORK HERE
+            curpos += 14;
         }
-        // THIS IS WHERE THE KEY/DATA LOOP ITERATION SHOULD START... WHILE (CURPOS < CURBLOCKSIZE
+        // THIS IS WHERE THE KEY/DATA LOOP ITERATION SHOULD START... WHILE (CURPOS < CURBLOCKSIZE)
+        qDebug() << "curoffset:" << curoffset << "curpos:" << curpos << "curblocksize:" << curblocksize;
+        while(curpos < curblocksize)
+        {
+            uint16_t curkeylength = qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + curpos, 2));
+            curpos += 2;
+            if(curkeylength == 0)
+            {
+                qDebug() << "keylength is wrong...";
+            }
+            qDebug() << "curkeylength:" << curkeylength; // in case i want to skip a record
+            uint32_t parentcnid = qFromBigEndian<uint32_t>(curimg->ReadContent(curoffset + curpos, 4));
+            qDebug() << "parentcnid:" << parentcnid;
+            curpos += 4;
+            uint16_t keynamelength = qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + curpos, 2));
+            curpos += 2;
+            qDebug() << "keynamelength:" << keynamelength;
+            if(keynamelength > 0)
+            {
+                QString keyfilename = "";
+                for(int j=0; j < keynamelength; j++)
+                {
+                    keyfilename += QString(QChar(qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + curpos + j*2, 2))));
+                }
+                curpos += 2*keynamelength;
+                qDebug() << "keyfilename:" << keyfilename;
+            }
+            //else // ALREADY SKIPPED KEYNAME LENGTH...
+            //    curpos += 2;
+            uint16_t datarecordtype = qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + curpos, 2));
+            curpos += 2;
+            qDebug() << "datarecordtype:" << datarecordtype;
+            if(datarecordtype == 0x01) // FOLDER RECORD 0x0001 - 88 bytes
+            {
+                qDebug() << "folder record";
+                curpos += 86;
+            }
+            else if(datarecordtype == 0x02) // FILE RECORD 0x0002 - 248 bytes
+            {
+                qDebug() << "file record";
+                curpos += 246;
+            }
+            else if(datarecordtype == 0x03 || datarecordtype == 0x04) // FOLDER THREAD RECORD 0x03/ FILE THREAD RECORD 0x04 - SKIP
+            {
+                // keylength should be 6 bytes...
+                // data length should another 6 bytes
+                curpos += 6;
+                uint16_t datanodesize = qFromBigEndian<uint16_t>(curimg->ReadContent(curoffset + curpos, 2));
+                curpos += 2;
+                qDebug() << "datanodesize:" << datanodesize;
+                curpos += 2*datanodesize;
+                //break;
+            }
+            else // SOMETHING IS WRONG PARSING CATALOG FILE
+            {
+                qDebug() << "error parsing catalog file!";
+                break;
+            }
+        }
     }
 }
