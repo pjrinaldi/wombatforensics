fa66299cd631b907fabfc81c773cadd23f4b9fb7
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-03-08 19:39:18 +0000

    changes from qt release and compile

--- ui_namefilter.h
+++ ui_namefilter.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'namefilter.ui'
 **
-** Created by: Qt User Interface Compiler version 5.15.2
+** Created by: Qt User Interface Compiler version 5.15.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
--- ui_settingsdialog.h
+++ ui_settingsdialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settingsdialog.ui'
 **
-** Created by: Qt User Interface Compiler version 5.15.2
+** Created by: Qt User Interface Compiler version 5.15.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
