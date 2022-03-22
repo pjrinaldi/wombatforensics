42d54562709cc94d5a66386eef53539310996fa2
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-03-22 14:42:16 +0000

    code cleanup for iso

--- iso9660.cpp
+++ iso9660.cpp
@@ -48,11 +48,6 @@ void ParseIsoDirectory(ForImg* curimg, uint32_t curstartsector, uint8_t ptreecnt
 	    while(!propfile.atEnd())
 	    {
 		QString line = propfile.readLine();
-		if(line.startsWith("PV" + QString::number(i) + " Path Table Size|"))
-		    pvpathtablesize.append(line.split("|").at(1).toUInt());
-		else if(line.startsWith("PV" + QString::number(i) + " Location of Occurrence of Type L Path Table|"))
-		    pvpathtableblk.append(line.split("|").at(1).toUInt());
-		
 		if(line.startsWith("PV" + QString::number(i) + " Data Length|"))
 		    pvrootdirlen.append(line.split("|").at(1).toUInt());
 		if(line.startsWith("PV" + QString::number(i) + " Extent Location|"))
@@ -65,11 +60,6 @@ void ParseIsoDirectory(ForImg* curimg, uint32_t curstartsector, uint8_t ptreecnt
 	    while(!propfile.atEnd())
 	    {
 		QString line = propfile.readLine();
-		if(line.startsWith("SV" + QString::number(i) + " Path Table Size|"))
-		    svpathtablesize.append(line.split("|").at(1).toUInt());
-		else if(line.startsWith("SV" + QString::number(i) + " Location of Occurrence of Type L Path Table|"))
-		    svpathtableblk.append(line.split("|").at(1).toUInt());
-
 		if(line.startsWith("SV" + QString::number(i) + " Data Length|"))
 		    svrootdirlen.append(line.split("|").at(1).toUInt());
 		if(line.startsWith("SV" + QString::number(i) + " Extent Location|"))
@@ -78,188 +68,34 @@ void ParseIsoDirectory(ForImg* curimg, uint32_t curstartsector, uint8_t ptreecnt
 	}
         propfile.close();
     }
-    //qDebug() << "pvrootdirblk:" << pvrootdirblk << "pvrootdirlen:" << pvrootdirlen << "svrootdirblk:" << svrootdirblk << "svrootdirlen:" << svrootdirlen;
-    //qDebug() << "curinode start:" << curinode;
     
     for(int i=0; i < pvrootdirblk.count(); i++)
     {
 	parsedblocks.append(pvrootdirblk.at(i));
 	curinode = ParseDirectoryContents(curimg, ptreecnt, pvrootdirblk.at(i), pvrootdirlen.at(i), 0, blocksize, false, curinode, "", &parsedblocks);
-	//qDebug() << "pv curinode:" << curinode;
     }
     for(int i=0; i < svrootdirblk.count(); i++)
     {
 	parsedblocks.append(svrootdirblk.at(i));
 	curinode = ParseDirectoryContents(curimg, ptreecnt, svrootdirblk.at(i), svrootdirlen.at(i), 0, blocksize, true, curinode, "", &parsedblocks);
-	//qDebug() << "sv curinode:" << curinode;
     }
-    //qDebug() << "curinode end:" << curinode;
-    //qDebug() << "parsed blocks:" << parsedblocks;
 
-    // what if i skip this operation and just for each pv/sv launch the root directory parse...
-    // parse each pv path table
-    /*
-    for(int i=0; i < pvpathtableblk.count(); i++)
-    {
-	uint64_t curoff = pvpathtableblk.at(i) * blocksize;
-	uint8_t lendi = qFromLittleEndian<uint8_t>(curimg->ReadContent(curoff, 1));
-	uint8_t extattrlen = qFromLittleEndian<uint8_t>(curimg->ReadContent(curoff + 1, 1));
-	uint32_t extblk = qFromLittleEndian<uint32_t>(curimg->ReadContent(curoff + 2, 4));
-	uint16_t pardirnum = qFromLittleEndian<uint16_t>(curimg->ReadContent(curoff + 6, 2));
-	QByteArray dirid = curimg->ReadContent(curoff + 8, lendi);
-	qDebug() << "curoff:" << curoff << "lendi:" << lendi << "extattrlen:" << extattrlen << "extblk:" << extblk << "pardirnum:" << pardirnum << "dirid:" << dirid;
-	ParseDirectoryContents(curimg, extblk, 0, blocksize, false);
-	//QString dirid = QString::fromStdString(curimg->ReadContent(curoff + 8, lendi).toStdString());
-    }
-    for(int i=0; i < svpathtableblk.count(); i++)
-    {
-	uint64_t curoff = svpathtableblk.at(i) * blocksize;
-	uint8_t lendi = qFromLittleEndian<uint8_t>(curimg->ReadContent(curoff, 1));
-	uint8_t extattrlen = qFromLittleEndian<uint8_t>(curimg->ReadContent(curoff + 1, 1));
-	uint32_t extblk = qFromLittleEndian<uint32_t>(curimg->ReadContent(curoff + 2, 4));
-	uint16_t pardirnum = qFromLittleEndian<uint16_t>(curimg->ReadContent(curoff + 6, 2));
-	QByteArray dirid = curimg->ReadContent(curoff + 8, lendi);
-	qDebug() << "curoff:" << curoff << "lendi:" << lendi << "extattrlen:" << extattrlen << "extblk:" << extblk << "pardirnum:" << pardirnum << "dirid:" << dirid;
-	ParseDirectoryContents(curimg, extblk, 0, blocksize, true);
-    }
-    */
-    // PROCESS IS TO PARSE SMALL PATH TABLE RECORD, WHICH LEADS TO THE DIRECTORY RECORD WHICH THEN TAKES ME TO THE FILE RECORD INFO???
-    //qDebug() << "pv start curinode:" << curinode;
-    /*
-    for(int i=0; i < pvpathtableblk.count(); i++)
-	curinode = ParsePathTable(curimg, ptreecnt, blocksize, pvpathtableblk.at(i), pvpathtablesize.at(i), curinode);
-    //qDebug() << "pv end curinode:" << curinode;
-    for(int i=0; i < svpathtableblk.count(); i++)
-	curinode = ParsePathTable(curimg, ptreecnt, blocksize, svpathtableblk.at(i), svpathtablesize.at(i), curinode);
-    */
-    //qDebug() << "sv end curinode:" << curinode;
-    /*
-    qDebug() << "parse iso:" << curstartsector << ptreecnt;
-    qDebug() << "pvcount:" << pvcount << "svcount:" << svcount;
-    qDebug() << "blocksize:" << blocksize;
-    qDebug() << "pvpathtablesize:" << pvpathtablesize << "pvpathtableblk:" << pvpathtableblk << "pvrootdirblk:" << pvrootdirblk << "pvrootdirlen:" << pvrootdirlen;
-    qDebug() << "svpathtablesize:" << svpathtablesize << "svpathtableblk:" << svpathtableblk << "svrootdirblk:" << svrootdirblk << "svrootdirlen:" << svrootdirlen;
-    */
 }
 
-quint64 ParsePathTable(ForImg* curimg, uint8_t ptreecnt, uint32_t blocksize, uint32_t ptblk, uint32_t ptlen, quint64 curinode)
-{
-    QHash<int, QString> idnodelink;
-    //qDebug() << "ptblk:" << ptblk << "ptlen:" << ptlen;
-    quint64 offset = ptblk * blocksize;
-    //qDebug() << "offset:" << offset;
-    uint32_t curoff = 0;
-    int idcnt = 1;
-    QString parpath = "/";
-    while(curoff < ptlen)
-    {
-	QString parentstr = QString("e" + curimg->MountPath().split("/").last().split("-e").last() + "-p" + QString::number(ptreecnt));
-	//qDebug() << "curoff:" << curoff;
-	//qDebug() << "fulloff:" << curoff + offset;
-	uint8_t lendi = qFromLittleEndian<uint8_t>(curimg->ReadContent(offset + curoff, 1));
-	//qDebug() << "lendi:" << lendi << "modulus:" << lendi % 2;
-	if(lendi == 0x00)
-	    break;
-	uint8_t extattrlen = qFromLittleEndian<uint8_t>(curimg->ReadContent(offset + curoff + 1, 1));
-	uint32_t extblk = qFromLittleEndian<uint32_t>(curimg->ReadContent(offset + curoff + 2, 4));
-	uint16_t pardirid = qFromLittleEndian<uint16_t>(curimg->ReadContent(offset + curoff + 6, 2));
-	QString dirid = QString::fromStdString(curimg->ReadContent(offset + curoff + 8, lendi).toStdString());
-	//qDebug() << "curinode:" << curinode << "dirid:" << dirid.toUtf8() << "extblk:" << extblk << "pardirid:" << pardirid;
-	// ADD NODE/PROPERTIES HERE FOR THE DIRECTORIES AND GET THEM IN THEIR PARENTS...
-	if(idcnt == 1) // root directory
-	{
-	    qDebug() << "curinode:" << curinode;
-	    //curinode = ParseDirectoryContents(curimg, ptreecnt, extblk, ?);
-	    //curinode = ParseDirectoryContents(curimg, ptreecnt, pvrootdirblk.at(i), pvrootdirlen.at(i), 0, blocksize, false, curinode, "", &parsedblocks);
-	    // parse root directory here...
-	}
-	if(idcnt > 1)
-	{
-	    idnodelink.insert(idcnt, QString::number(curinode) + "," + parpath + dirid + "/");
-	    if(idnodelink.contains(pardirid))
-	    {
-		//parpath = parpath + dirid + "/";
-		parpath = idnodelink.value(pardirid).split(",").at(1);
-		parentstr += QString("-f" + idnodelink.value(pardirid).split(",").at(0));
-	    }
-	    QHash<QString, QVariant> nodedata;
-	    nodedata.clear();
-	    nodedata.insert("name", QByteArray(dirid.toUtf8()).toBase64());
-	    nodedata.insert("path", QByteArray(parpath.toUtf8()).toBase64());
-	    nodedata.insert("id", QString("e" + curimg->MountPath().split("/").last().split("-e").last() + "-p" + QString::number(ptreecnt) + "-f" + QString::number(curinode)));
-	    mutex.lock();
-	    treenodemodel->AddNode(nodedata, parentstr, 3, 0);
-	    mutex.unlock();
-	    nodedata.clear();
-	    //qDebug() << "dirid:" << dirid << "parpath:" << parpath << "parentstr:" << parentstr;
-	    //dirlist->append(QString::number(idcnt) + "," + QString::number(curinode) + "," + QString::number(extblk) + "," + QString::number(pardirid));
-	    /*
-	    QTextStream out;
-	    QFile fileprop(curimg->MountPath() + "/p" + QString::number(ptreecnt) + "/f" + QString::number(curinode) + ".prop");
-	    if(!fileprop.isOpen())
-		fileprop.open(QIODevice::Append | QIODevice::Text);
-	    out.setDevice(&fileprop);
-	    out.flush();
-	    fileprop.close();
-	    if(idnodelink.contains(pardirid))
-		qDebug() << "dirid:" << dirid << "idcnt:" << idcnt << "paridcnt:" << pardirid << "parinode:" << idnodelink.value(pardirid);
-	    else
-		qDebug() << "dirid:" << dirid << "idcnt:" << idcnt << "paridcnt:" << pardirid << "parinode:" << "root and has no inode, -1";
-	    QHash<QString, QVariant> nodedata;
-	    nodedata.clear();
-	    nodedata.insert("name", QByteArray(dirid.toUtf8()).toBase64());
-	    nodedata.insert("path", QByteArray(filepath.toUtf8()).toBase64());
-	    nodedata.insert("size", datalen);
-	    if(parblk != 0)
-		parentstr += QString("-f" + QString::number(parinode));
-	    mutex.lock();
-	    treenodemodel->AddNode(nodedata, parentstr, itemtype, isdeleted);
-	    mutex.unlock();
-	    if(nodedata.value("id").toString().split("-").count() == 3)
-	    {
-		listeditems.append(nodedata.value("id").toString());
-		filesfound++;
-		isignals->ProgUpd();
-	    }
-	    nodedata.clear();
-	    */
-	    curinode++;
-	}
-	idcnt++;
-	curoff = curoff + 8 + lendi;
-	if(lendi % 2 != 0)
-	    curoff++;
-    }
-    //qDebug() << "idnodelink:" << idnodelink;
-    idnodelink.clear();
-    return curinode;
-}
 quint64 ParseDirectoryContents(ForImg* curimg, uint8_t ptreecnt, uint32_t dirextblk, uint32_t dirdatalen, uint32_t parblk, uint32_t blocksize, bool utf16, quint64 parinode, QString parpath, QList<uint32_t>* parsedblocks) // FILE/DIRECTORY RECORD
 {
-    //qDebug() << "parinode:" << parinode;
     quint64 curinode = parinode;
-    //if(parblk != 0 && parinode > 0)
     if(parblk != 0)
 	curinode = parinode + 1;
-    //qDebug() << "curinode:" << curinode;
-    qDebug() << "dirextblk:" << dirextblk << "dirdatalen:" << dirdatalen;
     uint64_t extblkoff = dirextblk * blocksize;
     uint16_t curoff = 0; // starting point which goes to 2048
-    //qDebug() << "parpath:" << parpath << "dirdatalen:" << dirdatalen;
-    //qDebug() << "dirdatalen blk cnt:" << dirdatalen / blocksize;
-    //qDebug() << "dirdatalen rem:" << dirdatalen % blocksize;
     int blkcnt = dirdatalen / blocksize;
     if(dirdatalen % blocksize != 0)
 	blkcnt++;
-    qDebug() << "blockcount:" << blkcnt;
-    //while(curoff <= dirdatalen)
     for(int i=0; i < blkcnt; i++)
     {
 	extblkoff = (dirextblk + i) * blocksize;
 	curoff = 0;
-	qDebug() << "i:" << i << "starting extblkoff:" << extblkoff;
-	//qDebug() << i << "blocks to loop over to capture all the files...";
-	// NEED TO PLACE THE WHILE WITHIN THIS FOR LOOP, AND THEN FIX EXTBLKOFF TO REFLECT THE NEXT BLOCK...
 	while(curoff < blocksize)
 	{
 	    uint8_t lendr = qFromLittleEndian<uint8_t>(curimg->ReadContent(extblkoff + curoff, 1));
@@ -307,7 +143,6 @@ quint64 ParseDirectoryContents(ForImg* curimg, uint8_t ptreecnt, uint32_t dirext
 		    fileid = fileid.left(fileid.indexOf(QChar(';')));
 		    QString curlayout = QString::number(extblk * blocksize) + "," + QString::number(physicalsize) + ";";
 		    uint8_t itemtype = 5;
-		    //uint8_t isdeleted = 0;
 		    QTextStream out;
 		    QFile fileprop(curimg->MountPath() + "/p" + QString::number(ptreecnt) + "/f" + QString::number(curinode) + ".prop");
 		    if(!fileprop.isOpen())
@@ -351,6 +186,11 @@ quint64 ParseDirectoryContents(ForImg* curimg, uint8_t ptreecnt, uint32_t dirext
 		    QString parentstr = QString("e" + curimg->MountPath().split("/").last().split("-e").last() + "-p" + QString::number(ptreecnt));
 		    if(parblk != 0)
 			parentstr += QString("-f" + QString::number(parinode));
+		    /*
+		    nodedata.insert("create", createdate);
+		    nodedata.insert("access", accessdate);
+		    nodedata.insert("modify", modifydate);
+		    */
 		    mutex.lock();
 		    treenodemodel->AddNode(nodedata, parentstr, itemtype, 0);
 		    mutex.unlock();
@@ -373,19 +213,6 @@ quint64 ParseDirectoryContents(ForImg* curimg, uint8_t ptreecnt, uint32_t dirext
 	    curoff = curoff + lendr;
 	}
     }
-    /*
-	curoff = curoff + lendr;
-    }
-    */
     return curinode;
 }
-        /*
-        nodedata.insert("create", createdate);
-        nodedata.insert("access", accessdate);
-        nodedata.insert("modify", modifydate);
-        nodedata.insert("id", QString("e" + curimg->MountPath().split("/").last().split("-e").last() + "-p" + QString::number(ptreecnt) + "-f" + QString::number(inodecnt)));
-        QString parentstr = QString("e" + curimg->MountPath().split("/").last().split("-e").last() + "-p" + QString::number(ptreecnt));
-        if(parinode > 0)
-            parentstr += QString("-f" + QString::number(parinode));
-        */
 
