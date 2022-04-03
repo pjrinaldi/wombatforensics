1f4877558070c507dd602ada67b56cc354bf3517
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-04-03 01:16:31 +0000

    working on mode for xfs

--- xfs.cpp
+++ xfs.cpp
@@ -55,6 +55,18 @@ void ParseXfs(ForImg* curimg, uint32_t curstartsector, uint8_t ptreecnt, uint32_
             #define S_IFBLK  0060000
             #define S_IFDIR  0040000
             #define S_IFCHR  0020000
+            #define S_IRWXU 00700
+            #define S_IRUSR 00400
+            #define S_IWUSR 00200
+            #define S_IXUSR 00100
+            #define S_IRWXG 00070
+            #define S_IRGRP 00040
+            #define S_IWGRP 00020
+            #define S_IXGRP 00010
+            #define S_IRWXO 00007
+            #define S_IROTH 00004
+            #define S_IWOTH 00002
+            #define S_IXOTH 00001
             XFS_DINODE_FMT_DEV,             // xfs_dev_t
             XFS_DINODE_FMT_LOCAL,           // bulk data
             XFS_DINODE_FMT_EXTENTS,         // struct xfs_bmbt_rec
