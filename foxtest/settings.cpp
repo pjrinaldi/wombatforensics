94eb8d08f6cb4a60022f7b4d4d5374185f32cfda
Pasquale Rinaldi <pjrinaldi@gmail.com>
2023-01-25 00:09:05 +0000

    settings layout is done, now i need to implement it.

--- foxtest/settings.cpp
+++ foxtest/settings.cpp
@@ -2,7 +2,7 @@
 
 FXIMPLEMENT(Settings,FXDialogBox,NULL, 0)
 
-Settings::Settings(FXWindow* parent, const FXString& title):FXDialogBox(parent, title, DECOR_TITLE|DECOR_BORDER|DECOR_CLOSE, 0, 0, 500, 290, 0,0,0,0, 0, 0)
+Settings::Settings(FXWindow* parent, const FXString& title):FXDialogBox(parent, title, DECOR_TITLE|DECOR_BORDER|DECOR_CLOSE, 0, 0, 480, 300, 0,0,0,0, 10, 10)
 {
     mainframe = new FXVerticalFrame(this, LAYOUT_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y, 0, 0, 0, 0, 10, 10, 10, 10);
     hframe1 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
@@ -13,12 +13,43 @@ Settings::Settings(FXWindow* parent, const FXString& title):FXDialogBox(parent, 
     thumbsizespinner->setIncrement(64);
     thumbsizespinner->setValue(128);
     hframe2 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    vidthumblabel = new FXLabel(hframe2, "Generate Video Thumbnail Every (%): ", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    new FXSpring(hframe2);
+    vidthumbspinner = new FXSpinner(hframe2, 5);
+    vidthumbspinner->setRange(10, 100);
+    vidthumbspinner->setIncrement(5);
+    vidthumbspinner->setValue(25);
     hframe3 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    casepathlabel = new FXLabel(hframe3, "Set Case Path:", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    new FXSpring(hframe3);
+    casepathtextfield = new FXTextField(hframe3, 40);
+    casepathbutton = new FXButton(hframe3, "Browse");
+    casepathtextfield->setText(FXString(getenv("HOME")) + "/WombatCases/");
     hframe4 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    reportpathlabel = new FXLabel(hframe4, "Set Report Path:", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    new FXSpring(hframe4);
+    reportpathtextfield = new FXTextField(hframe4, 40);
+    reportpathbutton = new FXButton(hframe4, "Browse");
+    reportpathtextfield->setText(FXString(getenv("HOME")) + "/Reports/");
     hframe5 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    reporttzlabel = new FXLabel(hframe5, "Report TimeZone:", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    reporttzcombo = new FXComboBox(hframe5, 45);
     hframe6 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    autosavelabel = new FXLabel(hframe6, "Set Autosave Interval (minutes):", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    new FXSpring(hframe6);
+    autosavespinner = new FXSpinner(hframe6, 5);
+    autosavespinner->setRange(1, 120);
+    autosavespinner->setIncrement(1);
+    autosavespinner->setValue(20);
     hframe7 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    carvingmanagerlabel = new FXLabel(hframe7, "Manage Carving File Types List:", NULL, LAYOUT_FILL_X|JUSTIFY_LEFT);
+    new FXSpring(hframe7);
+    carvingmanagerbutton = new FXButton(hframe7, "Manage");
     hframe8 = new FXHorizontalFrame(mainframe, LAYOUT_TOP|LAYOUT_FILL_X);
+    new FXLabel(hframe8, "", NULL, LAYOUT_FILL_X);
+    new FXSpring(hframe8);
+    cancelbutton = new FXButton(hframe8, "Cancel");
+    savebutton = new FXButton(hframe8, "Save");
     //mainimage = new FXPNGImage(this->getApp(), aboutwombat);
     //imgframe = new FXImageFrame(mainframe, mainimage, FRAME_NONE);
     /*
@@ -38,7 +69,7 @@ Settings::Settings(FXWindow* parent, const FXString& title):FXDialogBox(parent, 
     abouttext += " let me know and I will update this content accordingly.";
     mainlabel = new FXLabel(mainframe, abouttext, NULL, JUSTIFY_LEFT);
     */
-    this->setBackColor(FX::colorFromName("white"));
-    mainframe->setBackColor(FX::colorFromName("white"));
+    //this->setBackColor(FX::colorFromName("white"));
+    //mainframe->setBackColor(FX::colorFromName("white"));
     //mainlabel->setBackColor(FX::colorFromName("white"));
 }
