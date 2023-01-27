45bc1c6c88d6a0b42ffff72f46bc8664a8ae1e6c
Pasquale Rinaldi <pjrinaldi@gmail.com>
2023-01-27 18:04:40 +0000

    removed a few files i don't need anymore

--- foxtest/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -O3 -o wombatregistry wombatregistry.cpp icons.cpp managetags.cpp aboutbox.cpp viewer.cpp -lX11 -lXext -lXft -lfontconfig -lfreetype -lXcursor -lXrender -lXrandr -lXfixes -lXi -lGLU -lGL -ldl -lpthread -ljpeg -lrt -lpng -ltiff -lz -lbz2 libFOX-1.7.a libregf.a
