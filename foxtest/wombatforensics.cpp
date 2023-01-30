32d54a3124d689a6dcb88ffdbef826aa562e5b60
Pasquale Rinaldi <pjrinaldi@gmail.com>
2023-01-30 19:09:27 +0000

    ntfs is implemented for testing, need to clean up the code same as exfat code.

--- foxtest/wombatforensics.cpp
+++ foxtest/wombatforensics.cpp
@@ -2408,9 +2408,8 @@ FXString WombatForensics::GetFileSystemName(ForImg* curforimg, uint64_t offset)
 			{
 			    uint16_t singleletter = 0;
 			    ReadForImgContent(curforimg, &singleletter, rootdiroffset + curoffset + 2 + j*2);
-			    //std::cout << "singleletter: " << (char)singleletter << std::endl;
 			    partitionname += (char)singleletter;
-			    //partitionname += 
+			    //std::cout << "singleletter: " << (char)singleletter << std::endl;
 			}
 		    }
 		}
@@ -2420,7 +2419,103 @@ FXString WombatForensics::GetFileSystemName(ForImg* curforimg, uint64_t offset)
         }
         else if(FXString(fattype).find("NTFS") > -1)
         {
-            std::cout << "it's ntfs..." << std::endl;
+	    uint16_t bytespersector = 0;
+	    ReadForImgContent(curforimg, &bytespersector, offset + 11);
+	    uint8_t sectorspercluster = 0;
+	    curforimg->ReadContent(&sectorspercluster, offset + 13, 1);
+	    uint64_t mftstartingcluster = 0;
+	    ReadForImgContent(curforimg, &mftstartingcluster, offset + 48);
+	    //std::cout << "mft starting cluster: " << mftstartingcluster << std::endl;
+	    uint8_t mftentrysize = 0;
+	    curforimg->ReadContent(&mftentrysize, offset + 64, 1);
+	    uint64_t mftentrybytesize = (uint)mftentrysize * sectorspercluster * bytespersector;
+	    std::cout << "mftentrysize: " << (uint)mftentrysize << std::endl;
+	    std::cout << "mftentrybytesize: " << mftentrybytesize << std::endl;
+	    mftentrybytesize = 1024;
+	    uint64_t mftoffset = offset + mftstartingcluster * sectorspercluster * bytespersector;
+	    //std::cout << "mft offset: " << mftoffset << std::endl;
+	    char* mftsig = new char[4];
+	    curforimg->ReadContent((uint8_t*)mftsig, mftoffset + 3 * mftentrybytesize, 4);
+	    //std::cout << "mftsig: |" << mftsig << "|" << std::endl;
+	    if(FXString(mftsig).find("FILE") > -1) // proper mft entry
+	    {
+		uint16_t curoffset = 0;
+		ReadForImgContent(curforimg, &curoffset, mftoffset + 3*mftentrybytesize + 20);
+		uint16_t attrcnt = 0;
+		ReadForImgContent(curforimg, &attrcnt, mftoffset + 3*mftentrybytesize + 40);
+		//std::cout << "attrcnt:" << attrcnt << std::endl;
+		for(uint i=0; i < attrcnt; i++)
+		{
+		    uint32_t attrtype = 0;
+		    ReadForImgContent(curforimg, &attrtype, mftoffset + 3*mftentrybytesize + curoffset);
+		    //std::cout << "attr type: " << std::hex << attrtype << std::endl;
+		    if(attrtype == 0x60)
+			break;
+		    uint32_t attrlen = 0;
+		    ReadForImgContent(curforimg, &attrlen, mftoffset + 3*mftentrybytesize + curoffset + 4);
+		    //std::cout << "attrlen: " << attrlen << std::endl;
+		    if(attrlen == 0 || attrtype == 0xffffffff)
+			break;
+		    curoffset += attrlen;
+		    //std::cout << "curoffset: " << curoffset << std::endl;
+		}
+		//std::cout << "curoffset: " << curoffset;
+		uint32_t attrnamelength = 0;
+		ReadForImgContent(curforimg, &attrnamelength, mftoffset + 3*mftentrybytesize + curoffset + 16);
+		for(uint k=0; k < attrnamelength / 2; k++)
+		{
+		    uint16_t singleletter = 0;
+		    uint16_t attrnameoffset = 0;
+		    ReadForImgContent(curforimg, &attrnameoffset, mftoffset + 3*mftentrybytesize + curoffset + 20);
+		    ReadForImgContent(curforimg, &singleletter, mftoffset + 3*mftentrybytesize + curoffset + attrnameoffset + k*2);
+		    partitionname += (char)singleletter;
+		}
+		partitionname += " [NTFS]";
+		//std::cout << "partition name: " << partitionname.text() << std::endl;
+	    }
+	    /*
+	    if(QString::fromStdString(curimg->ReadContent(mftoffset + 3 * 1024, 4).toStdString()) == "FILE") // a proper MFT entry
+            {
+                int curoffset = qFromLittleEndian<uint16_t>(curimg->ReadContent(mftoffset + 3*1024 + 20, 2)); // offset to first attribute
+                for(uint i=0; i < qFromLittleEndian<uint16_t>(curimg->ReadContent(mftoffset + 3*1024 + 40, 2)); i++) // loop over attributes until get to next attribute id
+                {
+                    if(qFromLittleEndian<uint32_t>(curimg->ReadContent(mftoffset + 3*1024 + curoffset, 4)) == 0x60) // $VOLUME_NAME attribute to parse (always resident)
+                        break;
+                    curoffset += qFromLittleEndian<uint32_t>(curimg->ReadContent(mftoffset + 3*1024 + curoffset + 4, 4));
+                }
+                for(uint k=0; k < qFromLittleEndian<uint32_t>(curimg->ReadContent(mftoffset + 3*1024 + curoffset + 16, 4))/2; k++)
+                    partitionname += QString(QChar(qFromLittleEndian<uint16_t>(curimg->ReadContent(mftoffset + 3*1024 + curoffset + qFromLittleEndian<uint16_t>(curimg->ReadContent(mftoffset + 3*1024 + curoffset + 20, 2)) + k*2, 2))));
+                out << "Volume Label|" << partitionname << "|Volume Label for the file system." << Qt::endl;
+                partitionname += " [NTFS]";
+            }
+             */ 
+
+	    /*
+    //std::cout << "MFT Starting Cluster|Offset: " << mftstartingcluster << " | " << mftstartingcluster * sectorspercluster * bytespersector << std::endl;
+    // MFT ENTRY SIZE
+    //std::cout << "MFT Entry Size (cluster, bytes): " << (int)mftentrysize << ", " << mftentrysize * sectorspercluster * bytespersector << std::endl;
+    // MFT LAYOUT
+    uint64_t mftoffset = mftstartingcluster * sectorspercluster * bytespersector;
+    std::string mftlayout = GetDataAttributeLayout(rawcontent, curnt, mftoffset);
+    //curnt->mftlayout = mftlayout;
+    std::vector<std::string> mftlayoutlist;
+    mftlayoutlist.clear();
+    std::istringstream mll(mftlayout);
+    std::string mls;
+    uint64_t mftsize = 0;
+    while(getline(mll, mls, ';'))
+        mftlayoutlist.push_back(mls);
+    for(int i=0; i < mftlayoutlist.size(); i++)
+    {
+        std::size_t layoutsplit = mftlayoutlist.at(i).find(",");
+        mftsize += std::stoull(mftlayoutlist.at(i).substr(layoutsplit+1));
+    }
+    //std::cout << "MFT Size: " << mftsize << std::endl;
+    uint64_t maxmftentrycount = mftsize / (curnt->mftentrysize * curnt->sectorspercluster * curnt->bytespersector);
+    curnt->maxmftentrycount = maxmftentrycount;
+    //std::cout << "Max MFT Entry Count: " << maxmftentrycount << std::endl;
+	     */ 
+            //std::cout << "it's ntfs..." << std::endl;
             /*
             uint bytespercluster = qFromLittleEndian<uint16_t>(curimg->ReadContent(curstartsector*512 + 11, 2)) * qFromLittleEndian<uint8_t>(curimg->ReadContent(curstartsector*512 + 13, 1));
             qint64 mftoffset = curstartsector*512 + qFromLittleEndian<qulonglong>(curimg->ReadContent(curstartsector*512 + 48, 8)) * bytespercluster;
