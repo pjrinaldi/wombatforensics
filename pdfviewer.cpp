aa02d1eaa901dc70d3c26c551e45a40514dcb522
Pasquale Rinaldi <pjrinaldi@gmail.com>
2023-04-14 14:01:08 +0000

    pdfviewer is working. need to add zoom functionality, but i'll do that later. that is an enhancement, and i need to implement many other features and functionalities first.

--- pdfviewer.cpp
+++ pdfviewer.cpp
@@ -5,7 +5,6 @@ FXIMPLEMENT(PdfViewer,FXDialogBox, PdfViewerMap, ARRAYNUMBER(PdfViewerMap))
 
 PdfViewer::PdfViewer(FXWindow* parent, const FXString& title):FXDialogBox(parent, title, DECOR_TITLE|DECOR_RESIZE|DECOR_BORDER|DECOR_CLOSE, 0, 0, 400, 300, 0,0,0,0, 0, 0)
 {
-    //plainfont = new FXFont(this->getApp(), "monospace");
     vframe = new FXVerticalFrame(this, LAYOUT_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y, 0, 0, 0, 0, 0, 0, 0, 0);
     hframe = new FXHorizontalFrame(vframe, LAYOUT_TOP|LAYOUT_FILL_X);
     previcon = new FXPNGIcon(this->getApp(), back16);
@@ -19,26 +18,57 @@ PdfViewer::PdfViewer(FXWindow* parent, const FXString& title):FXDialogBox(parent
     imgview = new FXImageView(vframe, NULL, NULL, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y);
     prevbutton->disable();
     nextbutton->disable();
-    //newicon = new FXPNGIcon(this->getApp(), documentnew);
-    //newbutton = new FXButton(toolbar, "", newicon, this, ID_NEW, BUTTON_TOOLBAR|FRAME_RAISED, 0,0,0,0, 4,4,4,4);
-    //addbutton = new FXButton(hframe1, "Add", NULL, this, ID_ADDEVID, FRAME_RAISED|FRAME_THICK, 0, 0, 0, 0, 20, 20);
-    //textview = new FXText(vframe, NULL, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y);
-    //textview->setFont(plainfont);
-    //textview->setEditable(false);
 }
 
 long PdfViewer::LoadPrevPage(FXObject*, FXSelector, void*)
 {
-    std::cout << "load previous page if exists" << std::endl;
+    if(pagenum > 0)
+    {
+	pagenum--;
+	curfield->setText(FXString::value(pagenum + 1));
+	LoadPage(pagenum);
+	nextbutton->enable();
+	if(pagenum == 0)
+	    prevbutton->disable();
+    }
 
     return 1;
 }
 
 long PdfViewer::LoadNextPage(FXObject*, FXSelector, void*)
 {
-    std::cout << "load next page if exists" << std::endl;
+    if(pagenum < pagecnt - 1)
+    {
+	pagenum++;
+	curfield->setText(FXString::value(pagenum + 1));
+	LoadPage(pagenum);
+	prevbutton->enable();
+	if(pagenum == pagecnt - 1)
+	    nextbutton->disable();
+    }
 
     return 1;
+}
+
+void PdfViewer::LoadPage(int pnum)
+{
+    poppler::page* pdfpage = NULL;
+    poppler::image pdfimage;
+    poppler::page_renderer pdfrender;
+    pdfpage = pdfdoc->create_page(pnum);
+    pdfrender.set_render_hint(poppler::page_renderer::antialiasing, true);
+    pdfrender.set_render_hint(poppler::page_renderer::text_antialiasing, true);
+    pdfimage = pdfrender.render_page(pdfpage, 72.0, 72.0, -1, -1, -1, -1, poppler::rotate_0);
+    pdfimage.save("/tmp/wf/tmp.png", "PNG");
+    FXImage* img = new FXPNGImage(this->getApp(), NULL, IMAGE_KEEP|IMAGE_SHMI|IMAGE_SHMP);
+    FXFileStream stream;
+    this->getApp()->beginWaitCursor();
+    stream.open("/tmp/wf/tmp.png", FXStreamLoad);
+    img->loadPixels(stream);
+    stream.close();
+    img->create();
+    imgview->setImage(img);
+    this->getApp()->endWaitCursor();
 }
 
 void PdfViewer::LoadPdf(ForImg* curforimg, FileItem* curfileitem)
@@ -58,74 +88,16 @@ void PdfViewer::LoadPdf(ForImg* curforimg, FileItem* curfileitem)
     }
     pdfdoc = poppler::document::load_from_raw_data((char*)tmpbuf, curfileitem->size);
     pagecnt = pdfdoc->pages();
-    pagenum = 1;
-    curfield->setText(FXString::value(pagenum));
+    curfield->setText(FXString::value(pagenum + 1));
     totfield->setText(FXString::value(pagecnt));
-    if(pagenum < pagecnt)
+    if(pagenum < pagecnt - 1)
 	nextbutton->enable();
     // LOAD 1ST PAGE AS IMAGE
-    pdfpage = pdfdoc->create_page(0);
-    pdfrender.set_render_hint(poppler::page_renderer::antialiasing, true);
-    pdfrender.set_render_hint(poppler::page_renderer::text_antialiasing, true);
-    pdfimage = pdfrender.render_page(pdfpage, 72.0, 72.0, -1, -1, -1, -1, poppler::rotate_0);
-    pdfimage.save("/tmp/wf/tmp.png", "PNG");
-    FXImage* img = new FXPNGImage(this->getApp(), NULL, IMAGE_KEEP|IMAGE_SHMI|IMAGE_SHMP);
-    FXFileStream stream;
-    this->getApp()->beginWaitCursor();
-    stream.open("/tmp/wf/tmp.png", FXStreamLoad);
-    img->loadPixels(stream);
-    stream.close();
-    img->create();
-    imgview->setImage(img);
-    this->getApp()->endWaitCursor();
-    if(!inmemory)
-	fclose(tmpfile);
-    delete[] tmpbuf;
-    //std::cout << "inmemory: " << inmemory << std::endl;
+    LoadPage(0);
     /*
-    bool inmemory = true;
-    uint8_t* tmpbuf = NULL;
-    FILE* tmpfile;
-    std::string filecontents = "";
-    GetFileContent(curforimg, curfileitem, &inmemory, &tmpbuf, tmpfile);
-    ParseArtifact(curforimg, &currentitem, curfileitem, &inmemory, tmpbuf, tmpfile, &filecontents);
-    plaintext->setText(FXString(filecontents.c_str()));
+    // code cleanup
     if(!inmemory)
 	fclose(tmpfile);
     delete[] tmpbuf;
     */
 }
-
-/*
-void Viewer::GetText(FXString* buf)
-{
-    textview->getText(*buf);
-}
-
-void Viewer::GenerateReport(FXArray<FXString> taggedlist, std::vector<std::string> tags)
-{
-
-    // RENDER PDF TO AN IMAGE
-    pdfdoc = poppler::document::load_from_file("tmp.pdf");
-    pdfpage = pdfdoc->create_page(0);
-    pdfrender.set_render_hint(poppler::page_renderer::antialiasing, true);
-    pdfrender.set_render_hint(poppler::page_renderer::text_antialiasing, true);
-    pdfimage = pdfrender.render_page(pdfpage, 72.0, 72.0, -1, -1, -1, -1, poppler::rotate_0);
-    pdfimage.save("tmp.png", "PNG");
-    // Load Image
-    FXImage* img = new FXPNGImage(this->getApp(), NULL, IMAGE_KEEP|IMAGE_SHMI|IMAGE_SHMP);
-    FXFileStream stream;
-    this->getApp()->beginWaitCursor();
-    stream.open("tmp.png", FXStreamLoad);
-    img->loadPixels(stream);
-    stream.close();
-    img->create();
-    imgview->setImage(img);
-    this->getApp()->endWaitCursor();
-	poppler::document* pdfdoc;
-	poppler::page* pdfpage;
-	pdfdoc = poppler::document::load_from_raw_data((char*)tmpbuf, curfileitem->size);
-	int pagecount = pdfdoc->pages();
-	for(int i=0; i < pagecount; i++)
-	    filecontents->append(pdfdoc->create_page(i)->text().to_latin1());
-}*/
--- pdfviewer.h
+++ pdfviewer.h
@@ -34,9 +34,6 @@ class PdfViewer : public FXDialogBox
 	FXTextField* curfield;
 	FXTextField* totfield;
         poppler::document* pdfdoc;
-        poppler::page* pdfpage;
-        poppler::image pdfimage;
-        poppler::page_renderer pdfrender;
 	int pagenum = 0;
 	int pagecnt = 0;
 
@@ -52,21 +49,15 @@ class PdfViewer : public FXDialogBox
         };
         PdfViewer(FXWindow* parent, const FXString& title);
 	void LoadPdf(ForImg* curforimg, FileItem* curfileitem);
+	void LoadPage(int pnum);
 
 	long LoadPrevPage(FXObject*, FXSelector, void*);
 	long LoadNextPage(FXObject*, FXSelector, void*);
-        
-	//long AddEvidence(FXObject*, FXSelector, void*);
-        //void GenerateReport(FXArray<FXString> taggedlist, std::vector<std::string> tags);
-        //void GetText(FXString* buf);
 };
 
 FXDEFMAP(PdfViewer) PdfViewerMap[]={
     FXMAPFUNC(SEL_COMMAND, PdfViewer::ID_PREV, PdfViewer::LoadPrevPage),
     FXMAPFUNC(SEL_COMMAND, PdfViewer::ID_NEXT, PdfViewer::LoadNextPage),
-    //FXMAPFUNC(SEL_COMMAND, PdfViewer::ID_ADDEVID, EvidenceManager::AddEvidence),
-    //FXMAPFUNC(SEL_COMMAND, PdfViewer::ID_REMEVID, EvidenceManager::RemEvidence),
-    //FXMAPFUNC(SEL_SELECTED, PdfViewer::ID_EVIDLIST, EvidenceManager::EvidenceSelected),
 };
 
 #endif // PDFVIEWER_H
