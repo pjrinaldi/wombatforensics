07209609921bc7925dd9ef787517923a99b3ab42
Pasquale Rinaldi <pjrinaldi@protonmail.com>
2023-11-23 00:35:25 +0000

    determined the pathstring has multiple \0 terminators in the file, so i remove those and then the path string works properly. need to fix it when i populate the fileitem path variable now, i got it fixed in the file entry

--- wombatforensics.cpp
+++ wombatforensics.cpp
@@ -4205,19 +4205,32 @@ void WombatForensics::SortFileTable(std::vector<FileItem>* fileitems, FXString f
     {
         if(filecount == 0)
         {
+	    std::string tmpstring1 = fileitems->at(i).path;
+	    tmpstring1.erase(std::remove(tmpstring1.begin(), tmpstring1.end(), '\0'), tmpstring1.end());
+	    /*
 	    std::cout << "fileitem path size: " << fileitems->at(i).path.size() << std::endl;
 	    std::cout << "fileitem path: " << fileitems->at(i).path << std::endl;
-	    char* tmpchar = new char[fileitems->at(i).path.size() + 1];
+	    std::cout << "fileitem path without : " << tmpstring1 << std::endl;
+	    */
+	    //tmpfilestr.erase(std::remove(tmpfilestr.begin(), tmpfilestr.end(), '$'), tmpfilestr.end());
+	    char* tmpchar = new char[tmpstring1.size() + 1];
 	    std::cout << "char way: ";
-	    for(int j=0; j < fileitems->at(i).path.size(); j++)
+	    std::cout << tmpstring1.c_str() << std::endl;
+	    /*
+	    for(int j=0; j < tmpstring1; j++)
 	    {
-		std::cout << fileitems->at(i).path.at(j);
-		tmpchar[j] = fileitems->at(i).path.at(j);
+		if(tmpstring1.at(j) != '\0')
+		{
+		    //std::cout << " j: " << j << " is a string terminator" << std::endl;
+		    std::cout << tmpstring1.at(j);
+		    tmpchar[j] = fileitems->at(i).path.at(j);
+		}
 	    }
 	    tmpchar[fileitems->at(i).path.size()] = '\0';
 	    // not sure why the std string isn't converting to c_str() properly..
 	    std::cout << std::endl;
 	    std::cout << "tmpchar: " << tmpchar << std::endl;
+	    */
 
 	    //FXString testpath(fileitems->at(i).path.c_str());
 	    //std::cout << "fileitem path from fxstring: " << testpath.text() << std::endl;
@@ -4232,7 +4245,8 @@ void WombatForensics::SortFileTable(std::vector<FileItem>* fileitems, FXString f
             fileval += FXString::value(fileitems->at(i).isdirectory) + "|"; //  2
             fileval += FXString::value(fileitems->at(i).size) + "|"; //         3
             fileval += FXString(fileitems->at(i).name.c_str()) + "|"; //        4
-            fileval += FXString(fileitems->at(i).path.c_str()) + "|"; //        5
+            fileval += FXString(tmpstring1.c_str()) + "|"; //        5
+            //fileval += FXString(fileitems->at(i).path.c_str()) + "|"; //        5
             fileval += FXString(fileitems->at(i).create.c_str()) + "|"; //      6
             fileval += FXString(fileitems->at(i).access.c_str()) + "|"; //      7
             fileval += FXString(fileitems->at(i).modify.c_str()) + "|"; //      8
