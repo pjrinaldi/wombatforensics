cc6063084079d7d674303760b6082f965404a0ac
Pasquale Rinaldi <pjrinaldi@protonmail.com>
2023-12-23 22:50:09 +0000

    finished fat32 properties. need to test them out next

--- partitions.h
+++ partitions.h
@@ -17,7 +17,5 @@ void LoadGptPartitions(ForImg* curforimg, std::vector<std::string>* volnames, st
 void LoadExtendedPartitions(ForImg* curforimg, uint64_t epoffset, uint64_t epsize, std::vector<std::string>* volnames, std::vector<uint64_t>* volsizes, std::vector<uint64_t>* voloffsets);
 std::string GetFileSystemName(ForImg* curforimg, uint64_t offset);
 void GetVolumeProperties(ForImg* curforimg, uint64_t offset, std::vector<std::string>* volprops);
-//void GetNextCluster(ForImg* curimg, uint32_t clusternum, uint8_t fstype, uint64_t fatoffset, std::vector<uint>* clusterlist);
-//std::string ConvertBlocksToExtents(std::vector<uint> blocklist, uint blocksize, uint64_t rootdiroffset);
 
 #endif
