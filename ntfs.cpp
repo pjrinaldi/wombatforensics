5e9c9418b72ead90d855e1cdadc8a48526818fc6
Pasquale Rinaldi <pjrinaldi@protonmail.com>
2024-01-01 15:49:25 +0000

    working on parsing ntfs directory

--- ntfs.cpp
+++ ntfs.cpp
@@ -68,4 +68,219 @@ void LoadNtfsDirectory(CurrentItem* currentitem, std::vector<FileItem>* filevect
     std::string indexlayout = "";
     GetIndexAttributeLayout(currentitem->forimg, bytespercluster, mftentrybytes, mftentryoffset, &indexlayout); 
     std::cout << "index layout: " << indexlayout << std::endl;
+    // PARSE INDEX ROOT AND ALLOCATION TO DETERMINE THE DIR/FILE NAME/INODE AND SEE IF THEY MATCH
+    std::vector<std::string> indexlayoutlist;
+    indexlayoutlist.clear();
+    std::istringstream ill(indexlayout);
+    std::string ils;
+    while(getline(ill, ils, ';'))
+        indexlayoutlist.push_back(ils);
+    uint32_t indexrecordsize = 0;
+    for(int i=0; i < indexlayoutlist.size(); i++)
+    {
+        std::size_t layoutsplit = indexlayoutlist.at(i).find(",");
+        uint64_t indexoffset = std::stoull(indexlayoutlist.at(i).substr(0, layoutsplit));
+        uint64_t indexlength = std::stoull(indexlayoutlist.at(i).substr(layoutsplit+1));
+	if(i == 0) // $INDEX_ROOT
+	{
+	    // INDEX RECORD SIZE
+	    uint32_t indexrecordsize = 0;
+	    ReadForImgContent(currentitem->forimg, &indexrecordsize, indexoffset + 8);
+	    std::cout << "Index Record Size: " << indexrecordsize << std::endl;
+	    // STARTING OFFSET
+	    uint32_t startingoffset = 0;
+	    ReadForImgContent(currentitem->forimg, &startingoffset, indexoffset + 16);
+	    // ENDING OFFSET
+	    uint32_t endingoffset = 0;
+	    ReadForImgContent(currentitem->forimg, &endingoffset, indexoffset + 20);
+            /*
+            // ALLOCATION OFFSET - DELETED ENTRIES
+            uint32_t allocationoffset = 0;
+	    ReadForImgContent(currentitem->forimg, &allocationoffset, indexoffset + 24);
+            */
+	    uint32_t curpos = startingoffset;
+	    while(curpos < endingoffset)
+	    {
+		// INDEX ENTRY LENGTH
+		uint16_t indexentrylength = 0;
+		ReadForImgContent(currentitem->forimg, &indexentrylength, indexoffset + 16 + curpos + 8);
+                //std::cout << "Index Entry Length: " << indexentrylength << std::endl;
+	    }
+	}
+    }
+	/*
+            while(curpos < endoffset)
+            {
+                // FILE NAME ATTRIBUTE LENGTH
+                uint8_t* fnl = new uint8_t[2];
+                uint16_t filenamelength = 0;
+                ReadContent(rawcontent, fnl, indexoffset + 16 + curpos + 10, 2);
+                ReturnUint16(&filenamelength, fnl);
+                delete[] fnl;
+                //std::cout << "File Name Attribute Length: " << filenamelength << std::endl;
+                uint8_t* ief = new uint8_t[4];
+                uint32_t indexentryflags = 0;
+                ReadContent(rawcontent, ief, indexoffset + 16 + curpos + 12, 4);
+                ReturnUint32(&indexentryflags, ief);
+                delete[] ief;
+                //std::cout << "Index Entry Flags: 0x" << std::hex << indexentryflags << std::dec << std::endl;
+                if(indexentryflags & 0x02)
+                    break;
+                else
+                {
+                    if(indexentrylength > 0 && filenamelength > 66 && filenamelength < indexentrylength)
+                    {
+                        // I30 SEQUENCE ID
+                        uint8_t* i3si = new uint8_t[2];
+                        uint16_t i30seqid = 0;
+                        ReadContent(rawcontent, i3si, indexoffset + 16 + curpos + 6, 2);
+                        ReturnUint16(&i30seqid, i3si);
+                        delete[] i3si;
+                        //std::cout << "I30 Sequence ID: " << i30seqid << std::endl;
+                        // CHILD NT INODE
+                        uint8_t* cni = new uint8_t[6];
+                        uint64_t childntinode = 0;
+                        ReadContent(rawcontent, cni, indexoffset + 16 + curpos, 6);
+                        ReturnUint(&childntinode, cni, 6);
+                        delete[] cni;
+                        childntinode = childntinode & 0x00ffffffffffffff;
+                        //std::cout << "Child NT Inode: " << childntinode << std::endl;
+                        if(childntinode <= curnt->maxmftentrycount)
+                        {
+                            curpos = curpos + 16; // STARTING ON FILE_NAME ATTRIBUTE
+                            // FILE NAME TYPE
+                            uint8_t* fnt = new uint8_t[1];
+                            uint8_t fntype = 0;
+                            ReadContent(rawcontent, fnt, indexoffset + 16 + curpos + 65, 1);
+                            fntype = (uint8_t)fnt[0];
+                            delete[] fnt;
+                            //std::cout << "file name type: " << (int)fntype << std::endl;
+                            if(fntype != 0x02)
+                            {
+                                // FILE NAME LENGTH
+                                uint8_t* nl = new uint8_t[1];
+                                uint8_t namelength = 0;
+                                ReadContent(rawcontent, nl, indexoffset + 16 + curpos + 64, 1);
+                                namelength = (uint8_t)nl[0];
+                                delete[] nl;
+                                //std::cout << "name length: " << (int)namelength << std::endl;
+                                // FILE NAME
+                                std::string filename = "";
+                                for(uint8_t j=0; j < namelength; j++)
+                                {
+                                    uint8_t* sl = new uint8_t[2];
+                                    uint16_t singleletter = 0;
+                                    ReadContent(rawcontent, sl, indexoffset + 16 + curpos + 66 + j*2, 2);
+                                    ReturnUint16(&singleletter, sl);
+                                    delete[] sl;
+                                    filename += (char)singleletter;
+                                }
+                                //std::cout << "File Name to compare to child name: " << filename << " " << childpath << std::endl;
+                                if(filename.compare(childpath) == 0)
+                                    return childntinode;
+                            }
+                        }
+                    }
+                }
+                curpos += indexentrylength;
+            }
+        }
+        else // $INDEX_ALLOCATION
+        {
+            uint32_t indexrecordcount = indexlength / indexrecordsize;
+            //std::cout << "Index Record Count: " << indexrecordcount << std::endl;
+            uint64_t curpos = indexoffset;
+            for(uint32_t j=0; j < indexrecordcount; j++)
+            {
+                // START OFFSET
+                uint8_t* so = new uint8_t[4];
+                uint32_t startoffset = 0;
+                ReadContent(rawcontent, so, curpos + 24, 4);
+                ReturnUint32(&startoffset, so);
+                delete[] so;
+                //std::cout << "Start Offset: " << startoffset << std::endl;
+                // END OFFSET
+                uint8_t* eo = new uint8_t[4];
+                uint32_t endoffset = 0;
+                ReadContent(rawcontent, eo, curpos + 28, 4);
+                ReturnUint32(&endoffset, eo);
+                delete[] eo;
+                //std::cout << "End Offset: " << endoffset << std::endl;
+                curpos = curpos + 24 + startoffset + j*indexrecordsize;
+		//std::cout << "before while loop - curpos: " << curpos << " indexsize: " << indexoffset + j*indexrecordsize + indexrecordsize << std::endl;
+                while(curpos < indexoffset + j*indexrecordsize + indexrecordsize)
+                {
+		    // INDEX ENTRY LENGTH
+		    uint8_t* iel = new uint8_t[2];
+		    uint16_t indexentrylength = 0;
+		    ReadContent(rawcontent, iel, curpos + 8, 2);
+		    ReturnUint16(&indexentrylength, iel);
+		    delete[] iel;
+		    //std::cout << "Index Entry Length: " << indexentrylength << std::endl;
+		    // FILE NAME ATTRIBUTE LENGTH
+		    uint8_t* fnl = new uint8_t[2];
+		    uint16_t filenamelength = 0;
+		    ReadContent(rawcontent, fnl, curpos + 10, 2);
+		    ReturnUint16(&filenamelength, fnl);
+		    delete[] fnl;
+		    //std::cout << "File Name Attribute Length: " << filenamelength << std::endl;
+		    uint8_t* ief = new uint8_t[4];
+		    if(indexentrylength > 0 && filenamelength > 66 && filenamelength < indexentrylength)
+		    {
+			// I30 SEQUENCE ID
+			uint8_t* i3si = new uint8_t[2];
+			uint16_t i30seqid = 0;
+			ReadContent(rawcontent, i3si, curpos + 6, 2);
+			ReturnUint16(&i30seqid, i3si);
+			delete[] i3si;
+			//std::cout << "I30 Sequence ID: " << i30seqid << std::endl;
+			// CHILD NT INODE
+			uint8_t* cni = new uint8_t[6];
+			uint64_t childntinode = 0;
+			ReadContent(rawcontent, cni, curpos, 6);
+			ReturnUint(&childntinode, cni, 6);
+			delete[] cni;
+			childntinode = childntinode & 0x00ffffffffffffff;
+			//std::cout << "Child NT Inode: " << childntinode << std::endl;
+			if(childntinode <= curnt->maxmftentrycount)
+			{
+                            curpos = curpos + 16; // STARTING ON FILE_NAME ATTRIBUTE
+                            // FILE NAME TYPE
+                            uint8_t* fnt = new uint8_t[1];
+                            uint8_t fntype = 0;
+                            ReadContent(rawcontent, fnt, curpos + 65, 1);
+                            fntype = (uint8_t)fnt[0];
+                            delete[] fnt;
+                            //std::cout << "file name type: " << (int)fntype << std::endl;
+                            if(fntype != 0x02)
+                            {
+                                // FILE NAME LENGTH
+                                uint8_t* nl = new uint8_t[1];
+                                uint8_t namelength = 0;
+                                ReadContent(rawcontent, nl, curpos + 64, 1);
+                                namelength = (uint8_t)nl[0];
+                                delete[] nl;
+                                //std::cout << "name length: " << (int)namelength << std::endl;
+                                // FILE NAME
+                                std::string filename = "";
+                                for(uint8_t k=0; k < namelength; k++)
+                                {
+                                    uint8_t* sl = new uint8_t[2];
+                                    uint16_t singleletter = 0;
+                                    ReadContent(rawcontent, sl, curpos + 66 + k*2, 2);
+                                    ReturnUint16(&singleletter, sl);
+                                    delete[] sl;
+                                    filename += (char)singleletter;
+                                }
+                                //std::cout << "File Name to compare to child name: " << filename << " " << childpath << std::endl;
+                                if(filename.compare(childpath) == 0)
+                                    return childntinode;
+                            }
+			}
+		    }
+		    curpos += indexentrylength - 16;
+                }
+            }
+        }
+	 */ 
 }
--- ntfscommon.h
+++ ntfscommon.h
@@ -16,6 +16,8 @@ void GetDataAttributeLayout(ForImg* curimg, uint32_t bytespercluster, uint64_t m
 void GetIndexAttributeLayout(ForImg* curimg, uint32_t bytespercluster, uint64_t mftentrybytes, uint64_t offset, std::string* indexlayout); 
 void GetRunListLayout(ForImg* curimg, uint64_t offset, uint32_t bytespercluster, uint32_t attributelength, std::string* layout);
 
+//std::string ConvertWindowsTimeToUnixTimeUTC(uint64_t input);
+//std::string ConvertNtfsTimeToHuman(uint64_t ntfstime);
 /*
 std::string GetStandardInformationAttribute(std::ifstream* rawcontent, ntfsinfo* curnt, uint64_t mftentryoffset);
 std::string GetFileNameAttribute(std::ifstream* rawcontent, ntfsinfo* curnt, uint64_t mftentryoffset);
